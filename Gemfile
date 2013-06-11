2e684a5ea55aa8028b6168d8fa1a159259352de8
Destiny Makuyana <destiny.makuyana@gmail.com>
2013-06-11 10:59:22 +0100

    Add a full title helper

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ end
 
 group :test do 
   gem "capybara", "~> 2.1.0"
+  gem "spork", "~> 0.9.2"
 end
 
 # Gems used only for assets and not required
--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do 
+let(:base_title) { "Ruby on Rails Tutorial Sample App" } 
   
+    # --------- TEST HOME PAGE -------
     describe "Homepage" do 
 
         it " Should have the h1 'Sample App' " do 
@@ -9,12 +11,18 @@ describe "Static pages" do
            page.should have_selector('h1', :text => 'Sample App') 
         end
 
-        it " Should have title 'Sample App' " do 
+        it " Should not have long title '| Sample App' " do 
            visit '/static_pages/home'
-           page.should have_title('Ruby on Rails Tutorial Sample App | Home')
+           page.should_not have_title("#{base_title} | Home")
+        end
+
+        it "Should have base title " do 
+            visit '/static_pages/home'
+            page.should have_title("#{base_title}")
         end
     end
-
+    
+    # --------- TEST HELP PAGE -------
     describe "Help Page" do 
 
         it " Should have the h1 'Help' " do 
@@ -24,10 +32,11 @@ describe "Static pages" do
 
         it " Should have title 'Help' " do 
            visit '/static_pages/help'
-           page.should have_title('Ruby on Rails Tutorial Sample App | Help')
+           page.should have_title("#{base_title} | Help")
         end
-    end
+    end 
 
+    # --------- TEST ABOUT PAGE -------
     describe "About page" do 
 
         it " Should have the h1 'About' " do 
@@ -37,7 +46,21 @@ describe "Static pages" do
 
         it " Should have title 'About' " do 
            visit '/static_pages/about'
-           page.should have_title('Ruby on Rails Tutorial Sample App | About')
+           page.should have_title("#{base_title} | About")
+        end
+    end  
+
+    # --------- TEST CONTACT PAGE -------
+    describe "Contact Page" do 
+        
+        it "Should have h1 'Contact' " do 
+            visit '/static_pages/contact' 
+            page.should have_selector('h1', :text => 'Contact')
+        end
+
+        it "Should have title 'Contact' " do 
+            visit '/static_pages/contact'
+            page.should have_title("#{base_title} | Contact")
         end
     end
 end
